e0aaa26ba9b040ff8aee150667e194de937ef704
Templated User <templateduser@dummyemailZ123.com>
2024-06-27 16:36:27 +0100

    Baseline Baby

---
+++ administrator/cache/com_content/2aa650a104fe0263924a48df576efbb9-cache-com_content-35227601d68fe7734559295b26072831.php
@@ -0,0 +1,310 @@
+<?php die("Access Denied"); ?>#x#a:5:{s:4:"body";s:15919:"<div class="blog-featured">
+        <div class="page-header">
+        <h1>
+        Home        </h1>
+    </div>
+    
+            <div class="blog-items items-leading ">
+                            <div class="blog-item">
+                        
+<figure class="left item-image">
+            <img src="images/sampledata/cassiopeia/nasa1-1200.jpg" alt="Amazing Andromeda Galaxy" width="1200" height="400" loading="lazy">        </figure>
+
+<div class="item-content">
+    
+            <h2 class="item-title">
+                    <a href="/index.php?option=com_content&amp;view=article&amp;id=3:welcome-to-your-blog&amp;catid=8&amp;Itemid=102">
+                Welcome to your blog            </a>
+                </h2>
+    
+            
+    
+        
+        
+            <dl class="article-info text-muted">
+
+            <dt class="article-info-term">
+                            Details                    </dt>
+
+                    <dd class="createdby">
+    <span class="icon-user icon-fw" aria-hidden="true"></span>
+                    Written by: <span>Joomla</span>    </dd>
+        
+        
+                    <dd class="category-name">
+    <span class="icon-folder-open icon-fw" aria-hidden="true"></span>                        Category: <a href="/index.php?option=com_content&amp;view=category&amp;id=8&amp;Itemid=102">Blog</a>    </dd>
+        
+        
+                    <dd class="published">
+    <span class="icon-calendar icon-fw" aria-hidden="true"></span>
+    <time datetime="2024-06-27T15:01:19+00:00">
+        Published: 27 June 2024    </time>
+</dd>
+        
+    
+            
+        
+                    <dd class="hits">
+    <span class="icon-eye icon-fw" aria-hidden="true"></span>
+    <meta content="UserPageVisits:0">
+    Hits: 0</dd>
+            </dl>
+                    <ul class="tags list-inline">
+                                                                    <li class="list-inline-item tag-2 tag-list0">
+                    <a href="/index.php?option=com_tags&amp;view=tag&amp;id[0]=2:millions&amp;Itemid=101" class="btn btn-sm btn-info">
+                        Millions                    </a>
+                </li>
+                                                                                <li class="list-inline-item tag-3 tag-list1">
+                    <a href="/index.php?option=com_tags&amp;view=tag&amp;id[0]=3:worldwide&amp;Itemid=101" class="btn btn-sm btn-info">
+                        Worldwide                    </a>
+                </li>
+                                                                                <li class="list-inline-item tag-4 tag-list2">
+                    <a href="/index.php?option=com_tags&amp;view=tag&amp;id[0]=4:love&amp;Itemid=101" class="btn btn-sm btn-info">
+                        Love                    </a>
+                </li>
+                                                                                <li class="list-inline-item tag-5 tag-list3">
+                    <a href="/index.php?option=com_tags&amp;view=tag&amp;id[0]=5:joomla-5&amp;Itemid=101" class="btn btn-sm btn-info">
+                        Joomla 5                    </a>
+                </li>
+                        </ul>
+    
+        
+    <p>This is a sample blog posting.</p><p>If you log in to the site (the Administrator Login link is on the very bottom of this page) you will be able to edit it and all of the other existing articles. You will also be able to create a new article and make other changes to the site.</p><p>As you add and modify articles you will see how your site changes and also how you can customise it in various ways.</p><p>Go ahead, you can't break it.</p>
+    
+    
+    
+</div>
+
+                </div>
+                    </div>
+    
+                            <div class="blog-items ">
+                    <div class="blog-item">
+                    
+<figure class="left item-image">
+            <img src="images/sampledata/cassiopeia/nasa2-1200.jpg" alt="" width="1200" height="400" loading="lazy">        </figure>
+
+<div class="item-content">
+    
+            <h2 class="item-title">
+                    <a href="/index.php?option=com_content&amp;view=article&amp;id=4:about-your-home-page&amp;catid=8&amp;Itemid=121">
+                About your home page            </a>
+                </h2>
+    
+            
+    
+        
+        
+            <dl class="article-info text-muted">
+
+            <dt class="article-info-term">
+                            Details                    </dt>
+
+                    <dd class="createdby">
+    <span class="icon-user icon-fw" aria-hidden="true"></span>
+                    Written by: <span>Joomla</span>    </dd>
+        
+        
+                    <dd class="category-name">
+    <span class="icon-folder-open icon-fw" aria-hidden="true"></span>                        Category: <a href="/index.php?option=com_content&amp;view=category&amp;id=8&amp;Itemid=102">Blog</a>    </dd>
+        
+        
+                    <dd class="published">
+    <span class="icon-calendar icon-fw" aria-hidden="true"></span>
+    <time datetime="2024-06-27T15:01:19+00:00">
+        Published: 27 June 2024    </time>
+</dd>
+        
+    
+            
+        
+                    <dd class="hits">
+    <span class="icon-eye icon-fw" aria-hidden="true"></span>
+    <meta content="UserPageVisits:2">
+    Hits: 2</dd>
+            </dl>
+                    <ul class="tags list-inline">
+                                                                    <li class="list-inline-item tag-2 tag-list0">
+                    <a href="/index.php?option=com_tags&amp;view=tag&amp;id[0]=2:millions&amp;Itemid=101" class="btn btn-sm btn-info">
+                        Millions                    </a>
+                </li>
+                                                                                <li class="list-inline-item tag-3 tag-list1">
+                    <a href="/index.php?option=com_tags&amp;view=tag&amp;id[0]=3:worldwide&amp;Itemid=101" class="btn btn-sm btn-info">
+                        Worldwide                    </a>
+                </li>
+                                                                                <li class="list-inline-item tag-4 tag-list2">
+                    <a href="/index.php?option=com_tags&amp;view=tag&amp;id[0]=4:love&amp;Itemid=101" class="btn btn-sm btn-info">
+                        Love                    </a>
+                </li>
+                                                                                <li class="list-inline-item tag-5 tag-list3">
+                    <a href="/index.php?option=com_tags&amp;view=tag&amp;id[0]=5:joomla-5&amp;Itemid=101" class="btn btn-sm btn-info">
+                        Joomla 5                    </a>
+                </li>
+                        </ul>
+    
+        
+    <p>Your home page is set to display the four most recent articles from the blog category in a column. Then there are links to the next two oldest articles. You can change those numbers by editing the content options settings in the blog tab in your site administrator. There is a link to your site administrator if you are logged in.</p><p>If you want to have your blog post broken into two parts, an introduction and then a full length separate page, use the Read More span to insert a break.</p>
+    
+    
+        
+<p class="readmore">
+            <a class="btn btn-secondary" href="/index.php?option=com_content&amp;view=article&amp;id=4:about-your-home-page&amp;catid=8&amp;Itemid=121" aria-label="Read more: About your home page">
+            <span class="icon-chevron-right" aria-hidden="true"></span>            Read more: About your home page        </a>
+    </p>
+
+    
+    
+</div>
+
+<ul class="fields-container">
+    <li class="field-entry about-the-author ">    <span class="field-label ">About the Author: </span>
+<span class="field-value ">Uurnip greens yarrow ricebean endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale.</span>
+</li></ul>            </div>
+                    <div class="blog-item">
+                    
+<figure class="left item-image">
+            <img src="images/sampledata/cassiopeia/nasa3-1200.jpg" alt="" width="1200" height="400" loading="lazy">        </figure>
+
+<div class="item-content">
+    
+            <h2 class="item-title">
+                    <a href="/index.php?option=com_content&amp;view=article&amp;id=5:your-modules&amp;catid=8&amp;Itemid=102">
+                Your Modules            </a>
+                </h2>
+    
+            
+    
+        
+        
+            <dl class="article-info text-muted">
+
+            <dt class="article-info-term">
+                            Details                    </dt>
+
+                    <dd class="createdby">
+    <span class="icon-user icon-fw" aria-hidden="true"></span>
+                    Written by: <span>Joomla</span>    </dd>
+        
+        
+                    <dd class="category-name">
+    <span class="icon-folder-open icon-fw" aria-hidden="true"></span>                        Category: <a href="/index.php?option=com_content&amp;view=category&amp;id=8&amp;Itemid=102">Blog</a>    </dd>
+        
+        
+                    <dd class="published">
+    <span class="icon-calendar icon-fw" aria-hidden="true"></span>
+    <time datetime="2024-06-27T15:01:19+00:00">
+        Published: 27 June 2024    </time>
+</dd>
+        
+    
+            
+        
+                    <dd class="hits">
+    <span class="icon-eye icon-fw" aria-hidden="true"></span>
+    <meta content="UserPageVisits:0">
+    Hits: 0</dd>
+            </dl>
+                    <ul class="tags list-inline">
+                                                                    <li class="list-inline-item tag-2 tag-list0">
+                    <a href="/index.php?option=com_tags&amp;view=tag&amp;id[0]=2:millions&amp;Itemid=101" class="btn btn-sm btn-info">
+                        Millions                    </a>
+                </li>
+                                                                                <li class="list-inline-item tag-3 tag-list1">
+                    <a href="/index.php?option=com_tags&amp;view=tag&amp;id[0]=3:worldwide&amp;Itemid=101" class="btn btn-sm btn-info">
+                        Worldwide                    </a>
+                </li>
+                                                                                <li class="list-inline-item tag-4 tag-list2">
+                    <a href="/index.php?option=com_tags&amp;view=tag&amp;id[0]=4:love&amp;Itemid=101" class="btn btn-sm btn-info">
+                        Love                    </a>
+                </li>
+                                                                                <li class="list-inline-item tag-5 tag-list3">
+                    <a href="/index.php?option=com_tags&amp;view=tag&amp;id[0]=5:joomla-5&amp;Itemid=101" class="btn btn-sm btn-info">
+                        Joomla 5                    </a>
+                </li>
+                        </ul>
+    
+        
+    <p>Your site has some commonly used modules already preconfigured. These include:</p><ul><li>Image (type: Custom), which holds the image beneath the menu. This is a Custom module that you can edit to change the image.</li><li>Popular Tags (type: Tags - Popular), which will appear if you use tagging on your articles. Enter a tag in the Tags field when editing.</li><li>Older Posts (type: Articles - Category), which lists out articles by categories.</li><li>Syndication (type: Syndication Feeds), which allows your readers to read your posts in a news reader.</li><li>Login Form (type: Login), which allows your users to access restricted areas of the website.</li></ul><p>Each of these modules has many options which you can experiment with in the Module Manager in your site Administrator. When you are logged in you can also select the edit icon in the top right corner which will take you to an edit screen for that module. Always be sure to save and close any module you edit.</p><p>Joomla! also includes many other modules you can incorporate in your site. As you develop your site you may want to add more modules that you can find at the <a href="https://extensions.joomla.org/">Joomla Extensions Directory.</a></p>
+    
+    
+    
+</div>
+
+            </div>
+                    <div class="blog-item">
+                    
+<figure class="left item-image">
+            <img src="images/sampledata/cassiopeia/nasa4-1200.jpg" alt="" width="1200" height="400" loading="lazy">        </figure>
+
+<div class="item-content">
+    
+            <h2 class="item-title">
+                    <a href="/index.php?option=com_content&amp;view=article&amp;id=6:your-template&amp;catid=8&amp;Itemid=102">
+                Your Template            </a>
+                </h2>
+    
+            
+    
+        
+        
+            <dl class="article-info text-muted">
+
+            <dt class="article-info-term">
+                            Details                    </dt>
+
+                    <dd class="createdby">
+    <span class="icon-user icon-fw" aria-hidden="true"></span>
+                    Written by: <span>Joomla</span>    </dd>
+        
+        
+                    <dd class="category-name">
+    <span class="icon-folder-open icon-fw" aria-hidden="true"></span>                        Category: <a href="/index.php?option=com_content&amp;view=category&amp;id=8&amp;Itemid=102">Blog</a>    </dd>
+        
+        
+                    <dd class="published">
+    <span class="icon-calendar icon-fw" aria-hidden="true"></span>
+    <time datetime="2024-06-27T15:01:19+00:00">
+        Published: 27 June 2024    </time>
+</dd>
+        
+    
+            
+        
+                    <dd class="hits">
+    <span class="icon-eye icon-fw" aria-hidden="true"></span>
+    <meta content="UserPageVisits:0">
+    Hits: 0</dd>
+            </dl>
+                    <ul class="tags list-inline">
+                                                                    <li class="list-inline-item tag-2 tag-list0">
+                    <a href="/index.php?option=com_tags&amp;view=tag&amp;id[0]=2:millions&amp;Itemid=101" class="btn btn-sm btn-info">
+                        Millions                    </a>
+                </li>
+                                                                                <li class="list-inline-item tag-3 tag-list1">
+                    <a href="/index.php?option=com_tags&amp;view=tag&amp;id[0]=3:worldwide&amp;Itemid=101" class="btn btn-sm btn-info">
+                        Worldwide                    </a>
+                </li>
+                                                                                <li class="list-inline-item tag-4 tag-list2">
+                    <a href="/index.php?option=com_tags&amp;view=tag&amp;id[0]=4:love&amp;Itemid=101" class="btn btn-sm btn-info">
+                        Love                    </a>
+                </li>
+                                                                                <li class="list-inline-item tag-5 tag-list3">
+                    <a href="/index.php?option=com_tags&amp;view=tag&amp;id[0]=5:joomla-5&amp;Itemid=101" class="btn btn-sm btn-info">
+                        Joomla 5                    </a>
+                </li>
+                        </ul>
+    
+        
+    <p>Templates control the look and feel of your website.</p><p>This blog is installed with the Cassiopeia template.</p><p>You can edit the options by selecting the Working on Your Site, Template Settings link in the menu which is visible when you log in.</p><p>For example you can change the site background colour, highlights colour, site title, site description and title font used.</p><p>More options are available in the site administrator. You may also install a new template using the extension manager.</p>
+    
+    
+    
+</div>
+
+            </div>
+                </div>
+    
+    
+    
+</div>
+";s:4:"head";a:13:{s:5:"title";s:4:"Home";s:11:"description";s:65:"Joomla! - the dynamic portal engine and content management system";s:4:"link";s:0:"";s:8:"metaTags";a:2:{s:10:"http-equiv";a:1:{s:12:"content-type";s:24:"text/html; charset=utf-8";}s:4:"name";a:1:{s:6:"rights";N;}}s:5:"links";a:2:{s:91:"/index.php?option=com_content&amp;view=featured&amp;Itemid=101&amp;format=feed&amp;type=rss";a:3:{s:8:"relation";s:9:"alternate";s:7:"relType";s:3:"rel";s:7:"attribs";a:2:{s:4:"type";s:19:"application/rss+xml";s:5:"title";s:4:"Home";}}s:92:"/index.php?option=com_content&amp;view=featured&amp;Itemid=101&amp;format=feed&amp;type=atom";a:3:{s:8:"relation";s:9:"alternate";s:7:"relType";s:3:"rel";s:7:"attribs";a:2:{s:4:"type";s:20:"application/atom+xml";s:5:"title";s:4:"Home";}}}s:11:"styleSheets";a:0:{}s:5:"style";a:0:{}s:7:"scripts";a:0:{}s:6:"script";a:0:{}s:6:"custom";a:0:{}s:10:"scriptText";a:0:{}s:13:"scriptOptions";a:0:{}s:12:"assetManager";a:2:{s:13:"registryFiles";a:8:{i:0;s:30:"media/vendor/joomla.asset.json";i:1;s:30:"media/system/joomla.asset.json";i:2;s:30:"media/legacy/joomla.asset.json";i:3;s:46:"media/plg_system_guidedtours/joomla.asset.json";i:4;s:35:"media/com_content/joomla.asset.json";i:5;s:38:"templates/cassiopeia/joomla.asset.json";i:6;s:41:"media/plg_behaviour_compat/es5.asset.json";i:7;s:45:"media/plg_behaviour_compat/removed.asset.json";}s:6:"assets";a:0:{}}}s:13:"mime_encoding";s:9:"text/html";s:7:"pathway";a:0:{}s:6:"module";a:0:{}}
---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2024-06-27 14:53:43 UTC
+#Software: Joomla! 5.1.1 Stable [ Kudumisha ] 28-May-2024 16:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2024-06-27T14:53:43+00:00	INFO 86.29.224.215	joomlafailure	Empty password not allowed.
